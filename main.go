6c0c49ad9df5e1e3a590221a378e92a8fcbf76fc
A3R0-01 <81471338+A3R0-01@users.noreply.github.com>
2024-03-18 11:05:04 +0200

    api handlers and updates

--- main.go
+++ main.go
@@ -1,20 +1,25 @@
 package main
 
 import (
-	"context"
-	"fmt"
+	"log"
+	"os"
 
-	"github.com/A3R0-01/head-hunter/db"
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/mongo/gridfs"
+	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	db := db.NewMongoClient()
-	bucket, err := gridfs.NewBucket(db.Database("test_db"))
-	if err != nil {
-		panic(err)
-	}
+	// client := db.NewMongoClient()
+
+	app := fiber.New()
+
+	app.Listen(os.Getenv("HTTP_LISTEN_ADDRESS"))
+
+	// db := db.NewMongoClient()
+	// bucket, err := gridfs.NewBucket(db.Database("test_db"))
+	// if err != nil {
+	// 	panic(err)
+	// }
 	// file, err := os.Open("CV adapt.pdf")
 	// uploadOpts := options.GridFSUpload().SetMetadata(bson.M{"user": 893838})
 	// objectID, err := bucket.UploadFromStream("CV adapt.pdf", io.Reader(file), uploadOpts)
@@ -22,24 +27,30 @@ func main() {
 	// if err != nil {
 	// 	panic(err)
 	// }
-	filter := bson.M{"metadata": bson.M{"user": 893838}}
-	type gridfsFile struct {
-		Name     string `bson:"filename"`
-		Length   int64  `bson:"length"`
-		Metadata struct {
-			User int64 `bson:"user"`
-		} `bson:"metadata"`
-	}
-	cursor, err := bucket.Find(filter)
-	if err != nil {
-		panic(err)
-	}
-	var foundFiles []gridfsFile
-	if err = cursor.All(context.TODO(), &foundFiles); err != nil {
-		panic(err)
-	}
-	for _, file := range foundFiles {
-		fmt.Printf("filename: %s, length: %d\n, Name:%d", file.Name, file.Length, file.Metadata.User)
-	}
+	// filter := bson.M{"metadata": bson.M{"User": 893838}}
+	// type gridfsFile struct {
+	// 	Name     string `bson:"filename"`
+	// 	Length   int64  `bson:"length"`
+	// 	Metadata struct {
+	// 		User int64 `bson:"User"`
+	// 	} `bson:"metadata"`
+	// }
+	// cursor, err := bucket.Find(filter)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// var foundFiles []gridfsFile
+	// if err = cursor.All(context.TODO(), &foundFiles); err != nil {
+	// 	panic(err)
+	// }
+	// for _, file := range foundFiles {
+	// 	fmt.Printf("filename: %s, length: %d\n, Name:%d", file.Name, file.Length, file.Metadata.User)
+	// }
 
+}
+
+func init() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal(err)
+	}
 }
