46311d8f79c7fac6dc227bc13e4e5e23fd61532b
A3R0-01 <81471338+A3R0-01@users.noreply.github.com>
2024-04-10 16:47:45 +0200

    UpdateVerificaton Params

--- types/verification.go
+++ types/verification.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"time"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
@@ -15,16 +16,16 @@ const (
 
 type Verification struct {
 	ID         primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
-	EmployeeID primitive.ObjectID `bson:"Employee,omitempty" json:"Employee,omitempty"`
-	CompanyID  primitive.ObjectID `bson:"Company,omitempty" json:"Company,omitempty"`
-	Document   primitive.ObjectID `bson:"Document" json:"Document"`
+	EmployeeID primitive.ObjectID `bson:"EmployeeID,omitempty" json:"EmployeeID,omitempty"`
+	CompanyID  primitive.ObjectID `bson:"CompanyID,omitempty" json:"CompanyID,omitempty"`
+	Document   primitive.ObjectID `bson:"DocumentID" json:"DocumentID"`
 	ValidTill  time.Time          `bson:"ValidTill" json:"ValidTill"`
 	Created    time.Time          `bson:"Created,omitempty" json:"Created,omitempty"`
 }
 
 type CreateVerificationParams struct {
-	EmployeeID string `json:"Employee,omitempty"`
-	CompanyID  string `json:"Company,omitempty"`
+	EmployeeID string `json:"EmployeeID,omitempty"`
+	CompanyID  string `json:"CompanyID,omitempty"`
 	Document   string `json:"Document"`
 }
 
@@ -48,4 +49,36 @@ func (c *CreateVerificationParams) FromParams() (*Verification, error) {
 		ValidTill:  time.Now().AddDate(DefaultVerificationYears, DefaultVerificationMonths, DefaultVerificationDays),
 		Created:    time.Now(),
 	}, nil
+}
+
+type UpdateVerificationParams struct {
+	EmployeeID string    `bson:"EmployeeID,omitempty" json:"EmployeeID,omitempty"`
+	Document   string    `bson:"DocumentID" json:"DocumentID"`
+	ValidTill  time.Time `bson:"ValidTill" json:"ValidTill"`
+}
+
+func (u *UpdateVerificationParams) Validate() error {
+	return nil
+}
+
+func (u *UpdateVerificationParams) ToUpdateMongo() (update bson.M, err error) {
+	update = bson.M{}
+	_, err = primitive.ObjectIDFromHex(u.EmployeeID)
+	if err != nil {
+		err = fmt.Errorf("invalid user")
+	} else {
+		update["EmployeeID"] = u.EmployeeID
+	}
+	_, err = primitive.ObjectIDFromHex(u.Document)
+	if err != nil {
+		err = fmt.Errorf("invalid file")
+	} else {
+		update["Document"] = u.Document
+	}
+	if u.ValidTill.Before(time.Now()) {
+		err = fmt.Errorf("invalid time")
+	} else {
+		update["ValidTill"] = u.ValidTill
+	}
+	return
 }
